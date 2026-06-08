ae17afb7d13e54a34a04ba08837fd956fde746d5
ilmagri <ilmagri@gmail.com>
2026-06-08 11:26:44 +0200

    Auto update

--- autoupload.bat
+++ autoupload.bat
@@ -2,12 +2,10 @@
 cd /d C:\WC2026\send
 
 "C:\Program Files\Git\cmd\git.exe" add .
-
 "C:\Program Files\Git\cmd\git.exe" diff --cached --quiet
-if %errorlevel%==0 (
-    echo No changes detected.
-    exit /b
-)
+if %errorlevel%==0 exit /b 0
 
 "C:\Program Files\Git\cmd\git.exe" commit -m "Auto update"
 "C:\Program Files\Git\cmd\git.exe" push
+
+exit /b 0
